c6b1273c9a5231343092fa5a344f15f45d386392
Henry <adinoyiotaru@gmail.com>
2023-06-16 15:50:11 -0700

    3-alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the last digit of a randomly generated number
+ * and provides additional information about the digit.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	       	putchar(letter);
+        }
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+		putchar(letter);
+	}
+
+        putchar('\n');
+
+        return (0);
+}
