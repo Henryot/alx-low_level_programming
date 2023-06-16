baf4aba49a288c7a3d62a54c054fb6a5e088c859
Henry <adinoyiotaru@gmail.com>
2023-06-16 15:41:41 -0700

    2-print alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - is the gate
- * return - the return value is always 0
+ * main - Entry point
+ *
+ * Description: Prints the last digit of a randomly generated number
+ * and provides additional information about the digit.
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int letter;
