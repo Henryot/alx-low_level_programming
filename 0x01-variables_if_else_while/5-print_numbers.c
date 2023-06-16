13d69df9373f3c1c46458819954f0ab76d35b779
Henry <adinoyiotaru@gmail.com>
2023-06-16 16:43:34 -0700

    5-print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the last digit of a randomly generated number
+ * and provides additional information about the digit.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+	{
+		printf("%d", number);
+	
+	}
+	
+	printf("\n");
+	return (0);
+
+}
