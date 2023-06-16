52ddcfb91bde1008e78d7304c1f94cacac14bfdc
Henry <adinoyiotaru@gmail.com>
2023-06-16 16:00:05 -0700

    3-alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,7 +4,7 @@
  * main - Entry point
  *
  * Description: Prints the last digit of a randomly generated number
- * and provides additional information about the digit.
+ * and provides additional information about the digit
  *
  * Return: Always 0 (Success)
  */
