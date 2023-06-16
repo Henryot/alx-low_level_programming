f5f7a8158440412ca0cca1eb69cd5a17769cb8c7
Henry <adinoyiotaru@gmail.com>
2023-06-16 15:23:28 -0700

    2-print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *
+ * main is the gate
+ * return is theend
+ */
+
+int main(){
+	int letter;
+
+	for (letter = 'a'; letter <= 'z'; letter ++){
+	
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
