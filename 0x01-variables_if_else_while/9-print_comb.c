31aa34fa33e9dd6822ad2f3d0b9e4265789506cd
Henry <adinoyiotaru@gmail.com>
2023-06-16 19:36:06 -0700

    last

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,24 +10,18 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
+int main(void) 
 {
-	int first_digit, second_digit;
-        
-	for (first_digit = '0'; first_digit <= '9'; first_digit++)
+	int digit;
+	for (digit = '0'; digit <= '9'; digit++)
 	{
-		for (second_digit = first_digit; second_digit <= '9'; second_digit++)
+		putchar(digit);
+		if (digit != '9') 
 		{
-			putchar(first_digit);
-			putchar(second_digit);
-
-			if (first_digit != '9' || second_digit != '9')
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
-		putchar('\n');
-		return (0);
+	putchar('\n');
+	return 0;
 }
