f9293cea157b068369abde774ff352a422ab6a69
Henry <adinoyiotaru@gmail.com>
2023-06-16 18:04:36 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the last digit of a randomly generated number
+ * and provides additional information about the digit.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit;
+	for (digit = 0; digit < 16; digit++)
+	{
+		if (digit < 10)
+		{
+			putchar(digit + '0');
+		}
+		else putchar(digit + 'a' - 10);
+
+	}
+
+	putchar('\n');
+	return (0);
+
+}
