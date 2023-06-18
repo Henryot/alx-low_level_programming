7b6beb78a9125df02403adb9442213df7a946388
Henry <adinoyiotaru@gmail.com>
2023-06-17 21:51:22 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Prints all possible combinations of two different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
 {
 	int digit1;
 	int digit2;
