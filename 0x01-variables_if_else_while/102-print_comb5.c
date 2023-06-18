dec67366fd94b52e43efbf799fb7bc61b460d7b9
Henry <adinoyiotaru@gmail.com>
2023-06-18 11:36:24 -0700

    last question

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,36 +1,41 @@
 #include <stdio.h>
-
-
-
 /**
  * main - Prints all possible combinations of two different digits,
  *        in ascending order, separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
-
-
 int main()
 {
 	int num1;
 	int num2;
+	int num3;
+	int num4;
 
-	for (num1 = 0; num1 <= 99; num1++)
+	for (num1 = 0; num1 <= 9; num1++)
 	{
-		for (num2 = num1 + 1; num2 <= 99; num2++)
+		for (num2 = 0; num2 <= 8; num2++)
 		{
-			if (num1 <= num2)
-			{
-				putchar((num1 / 10) + '0');
-				putchar((num1 % 10) + '0');
-				putchar(' ');
-				putchar((num2 / 10) + '0');
-				putchar((num2 % 10) + '0');
 
-				if (num1 != 99 || num2 != 99)
+			for (num3 = 0; num3 <= 9; num3++)
+			{
+				for (num4 = 0; num4 <= 9; num4++)
 				{
-					putchar(',');
-					putchar(' ');
+					if ((num1 * 10 + num2) <= (num3 * 10 + num4))
+					{
+						putchar(num1 + '0');
+						putchar(num2 + '0');
+						putchar(' ');
+						putchar(num3 + '0');
+						putchar(num4 + '0');
+						
+						if (num1 != 9 || num2 != 8 || num3 != 9 || num4 != 9)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+
+					}
 				}
 			}
 		}
