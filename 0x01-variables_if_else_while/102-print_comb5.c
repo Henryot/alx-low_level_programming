422260ce5f79e236c41534fd9dfdc8de64c7ae1c
Henry <adinoyiotaru@gmail.com>
2023-06-18 11:45:51 -0700

    last question

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,7 +14,7 @@ int main()
 
 	for (num1 = 0; num1 <= 9; num1++)
 	{
-		for (num2 = 0; num2 <= 8; num2++)
+		for (num2 = 0; num2 <= 9; num2++)
 		{
 
 			for (num3 = 0; num3 <= 9; num3++)
@@ -29,7 +29,7 @@ int main()
 						putchar(num3 + '0');
 						putchar(num4 + '0');
 						
-						if (num1 != 9 || num2 != 8 || num3 != 9 || num4 != 9)
+						if (num1 != 9 || num2 != 9 || num3 != 9 || num4 != 9)
 						{
 							putchar(',');
 							putchar(' ');
