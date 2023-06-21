db08994b4032b5dbc011144a864aaf51687fd6af
Henry <adinoyiotaru@gmail.com>
2023-06-21 11:18:30 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,7 +18,7 @@ void times_table(void)
 	if (z > 9)
 	{
 	u = z % 10;
-	d = (z - u)/10;
+	d = (z - u) / 10;
 	_putchar(44);
 	_putchar(32);
 	_putchar(d + '0');
@@ -30,7 +30,7 @@ void times_table(void)
 	{
 	_putchar(44);
 	_putchar(32);
-	_putchar(32);	
+	_putchar(32);
 	}
 	_putchar(z + '0');
 	}
