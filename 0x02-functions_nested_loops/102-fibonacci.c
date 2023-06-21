a9bcf2a810503d999dab5c19a2f43942026874b4
Henry <adinoyiotaru@gmail.com>
2023-06-21 16:49:28 -0700

    102 fibonnaci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,10 +11,11 @@ void print_fibonacci(int n)
 	int first = 1;
 	int second = 2;
 	int next;
+	int i;
 
 	printf("%d, %d, ", first, second);
 
-	for (int i = 3; i <= n; i++)
+	for (i = 3; i <= n; i++)
 	{
 		next = first + second;
 		printf("%d", next);
