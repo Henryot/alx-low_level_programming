cb85080464441adaa927562c341912add3b284b8
Henry <adinoyiotaru@gmail.com>
2023-06-22 03:06:15 -0700

    wc -l 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
 /**
 * main - causes an infinite loop
 * Return: 0
 */
-
 int main(void)
 {
 	int i;
@@ -13,7 +11,9 @@ int main(void)
 
 	i = 0;
 
-	/* while (i < 10)
+	/* Comment out the infinite loop */
+	/*
+	while (i < 10)
 	{
 		putchar(i);
 	}
