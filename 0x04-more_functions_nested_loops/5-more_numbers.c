8e97eac98d7d8576efb37c12a95181837bc4b6cc
Henry <adinoyiotaru@gmail.com>
2023-06-22 18:02:19 -0700

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_most - printing numbers and all
+ * Return: retun is always 0
+ */
+#include "main.h"
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
+		}
+
+		_putchar('\n');
+	}
+}
+
