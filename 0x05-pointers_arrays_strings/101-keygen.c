267751a041b2e2c17d9571f3b184c8c5c225dd06
Henry <adinoyiotaru@gmail.com>
2023-06-26 06:06:09 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,19 +6,21 @@
 
 char *generate_password()
 {
-    char *password = malloc(PASSWORD_LENGTH + 1);
+    char *password = malloc(PASSWORD_LENGTH + 1); // Allocate memory for password
     const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    int i;
+    int i, sum = 0;
 
-    srand(time(NULL));
+    srand(time(NULL)); // Seed the random number generator
 
-    for (i = 0; i < PASSWORD_LENGTH; i++)
+    for (i = 0; i < PASSWORD_LENGTH - 1; i++)
     {
         int index = rand() % (sizeof(charset) - 1);
         password[i] = charset[index];
+        sum += charset[index];
     }
 
-    password[PASSWORD_LENGTH] = '\0';
+    password[PASSWORD_LENGTH - 1] = (sum % 10) + '0'; // Last character should be a digit
+    password[PASSWORD_LENGTH] = '\0'; // Add null terminator
 
     return password;
 }
@@ -27,9 +29,9 @@ int main()
 {
     char *password = generate_password();
 
-    printf("%s", password);
+    printf("%s\n", password);
 
-    free(password);
+    free(password); // Free allocated memory
 
     return 0;
 }
