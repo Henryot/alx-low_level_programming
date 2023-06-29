cf104e3952b779dc01b98d202dd36176c09a5a96
Henry <adinoyiotaru@gmail.com>
2023-06-29 02:27:10 -0700

    103-main.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * infinite_add - Adds two numbers
+ * @n1: First number as a string
+ * @n2: Second number as a string
+ * @r: Buffer to store the result
+ * @size_r: Size of the buffer
+ *
+ * Return: Pointer to the result
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int len1 = 0, len2 = 0, sum = 0, carry = 0, i = 0;
+
+	/* Calculate the lengths of n1 and n2 */
+	while (n1[len1] != '\0')
+		len1++;
+	while (n2[len2] != '\0')
+		len2++;
+
+	/* Check if the result can be stored in r */
+	if (len1 >= size_r || len2 >= size_r)
+		return (0);
+
+	len1--;
+	len2--;
+
+	/* Perform addition digit by digit */
+	while (len1 >= 0 || len2 >= 0)
+	{
+		sum = carry;
+
+		if (len1 >= 0)
+			sum += (n1[len1] - '0');
+		if (len2 >= 0)
+			sum += (n2[len2] - '0');
+
+		carry = sum / 10;
+		sum %= 10;
+
+		r[i] = sum + '0';
+
+		i++;
+		len1--;
+		len2--;
+	}
+
+	if (carry)
+	{
+		/* Check if there is enough space for the carry */
+		if (i >= size_r)
+			return (0);
+
+		r[i] = carry + '0';
+		i++;
+	}
+
+	/* Reverse the result in r */
+	reverse_string(r, i);
+
+	r[i] = '\0';
+
+	return (r);
+}
+
+/**
+ * reverse_string - Reverses a string
+ * @str: The string to be reversed
+ * @len: Length of the string
+ */
+void reverse_string(char *str, int len)
+{
+	int start = 0;
+	int end = len - 1;
+	char temp;
+
+	while (start < end)
+	{
+		temp = str[start];
+		str[start] = str[end];
+		str[end] = temp;
+
+		start++;
+		end--;
+	}
+}
+
