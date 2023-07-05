a37272cae5d2f15b7ff53f77fe97d4819e0bed55
Henry <adinoyiotaru@gmail.com>
2023-07-05 05:45:58 -0700

    5-sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,30 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * _sqrt_recursion - Returns the natural square root of a number.
- * @n: The number to calculate the square root of.
- *
- * Return: The natural square root of the number.
- *         If n does not have a natural square root, returns -1.
- */
-int _sqrt_recursion(int n);
 int sqrt_helper(int n, int start, int end);
 
+/**
+ * _sqrt_recursion - Calculates the square root of a number.
+ * @n: The number to calculate the square root of.
+ *
+ * Return: The square root of the number if it is a perfect square,
+ *         -1 otherwise.
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-		return (-1); /* If n is negative, return -1 to indicate an error */
-	else if (n == 0 || n == 1)
-		return (n); /* Base case: square root of 0 or 1 is the number itself */
-	else
-		return (sqrt_helper(n, 1, n));
+		return (-1);
+
+	return (sqrt_helper(n, 1, n));
 }
 
 /**
- * sqrt_helper - Recursive helper function to calculate the square root.
+ * sqrt_helper - Recursive helper function to calculate square root.
  * @n: The number to calculate the square root of.
- * @start: The starting point for the search.
- * @end: The ending point for the search.
+ * @start: The starting point of the range to search for the square root.
+ * @end: The ending point of the range to search for the square root.
  *
- * Return: The square root of the number.
+ * Return: The square root of the number if it is a perfect square,
+ *         -1 otherwise.
  */
 int sqrt_helper(int n, int start, int end)
 {
@@ -35,13 +33,13 @@ int sqrt_helper(int n, int start, int end)
 	if (start > end)
 		return (-1);
 
-	mid = (start + end) / 2; /* Calculate the middle point */
+	mid = (start + end) / 2;
 
 	if (mid * mid == n)
 		return (mid);
-	else if (mid * mid > n)
-		return (sqrt_helper(n, start, mid - 1));
-	else
-		return (sqrt_helper(n, mid + 1, end));
-}
 
+	if (mid * mid > n)
+		return (sqrt_helper(n, start, mid - 1));
+
+	return (sqrt_helper(n, mid + 1, end));
+}
