b5f5219e690f1761a957df87dd752e1623a04578
Henry <adinoyiotaru@gmail.com>
2023-07-05 05:59:54 -0700

    100-palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,5 @@ int sqrt_helper(int n, int start, int end);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_prime_helper(int n, int divisor);
+int is_palindrome(char *s);
+int is_palindrome_helper(char *s, int start, int end);
