cb2054ad963dd20fbd03311d18f795888b0994f3
Henry <adinoyiotaru@gmail.com>
2023-07-07 02:46:51 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc; /* Silence unused parameter warning */
+	printf("%s\n", argv[0]);
+	return 0;
+}
