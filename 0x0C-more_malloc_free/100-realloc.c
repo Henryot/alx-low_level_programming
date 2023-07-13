826bb1e338f247dc214d5a4df97ec77bb0e13072
Henry <adinoyiotaru@gmail.com>
2023-07-13 11:03:27 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -31,6 +31,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr != NULL)
 	{
 		unsigned int copy_size = old_size;
+
 		if (new_size < old_size)
 			copy_size = new_size;
 
