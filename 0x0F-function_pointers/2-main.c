e124736a5e7918f1bd21211ddc59619e282def6c
Henry <adinoyiotaru@gmail.com>
2023-07-19 10:55:16 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-main.c
+++ 0x0F-function_pointers/2-main.c
@@ -2,18 +2,18 @@
 #include "function_pointers.h"
 
 /**
- * is_98 - Checks if a number is equal to 98.
+ * is_98 - Check if a number is equal to 98.
  * @elem: The integer to check.
  *
  * Return: 0 if false, something else otherwise.
  */
 int is_98(int elem)
 {
-	return (98 == elem);
+	return (elem == 98);
 }
 
 /**
- * is_strictly_positive - Checks if a number is greater than 0.
+ * is_strictly_positive - Check if a number is greater than 0.
  * @elem: The integer to check.
  *
  * Return: 0 if false, something else otherwise.
@@ -24,7 +24,7 @@ int is_strictly_positive(int elem)
 }
 
 /**
- * abs_is_98 - Checks if the absolute value of a number is 98.
+ * abs_is_98 - Check if the absolute value of a number is 98.
  * @elem: The integer to check.
  *
  * Return: 0 if false, something else otherwise.
@@ -41,7 +41,8 @@ int abs_is_98(int elem)
  */
 int main(void)
 {
-	int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
+	int array[20] = {0, -98, 98, 402, 1024, 4096, -1024, -98, 1, 2,
+			 3, 4, 5, 6, 7, 8, 9, 10, 11, 98};
 	int index;
 
 	index = int_index(array, 20, is_98);
@@ -50,5 +51,6 @@ int main(void)
 	printf("%d\n", index);
 	index = int_index(array, 20, is_strictly_positive);
 	printf("%d\n", index);
+
 	return (0);
 }
