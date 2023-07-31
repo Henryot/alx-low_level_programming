fbc5f31bb60ed944155f3af9fe9430995446fdbc
Henry <adinoyiotaru@gmail.com>
2023-07-31 10:54:23 -0700

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,13 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
+#include <stddef.h>
+
+/* Struct for a singly linked list of integers */
 typedef struct listint_s
 {
 	int n;
@@ -26,4 +22,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+
 #endif /* LISTS_H */
+
