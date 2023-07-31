5788fde354234bab3710e6ce6f4eb88ae2ec8361
Henry <adinoyiotaru@gmail.com>
2023-07-31 11:31:58 -0700

    103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -26,10 +26,10 @@ listint_t *find_listint_loop(listint_t *head)
 				slow = slow->next;
 				fast = fast->next;
 			}
-			return slow;
+			return (slow);
 		}
 	}
 
 	/* No loop */
-	return NULL;
+	return (NULL);
 }
