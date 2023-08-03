943d5b5c5ace343e593f5a298c06b1b011294509
Henry <adinoyiotaru@gmail.com>
2023-08-03 14:05:06 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_binary(0);
+	_putchar('\n');
+
+	print_binary(1);
+	_putchar('\n');
+
+	print_binary(98);
+	_putchar('\n');
+
+	print_binary(1024);
+	_putchar('\n');
+
+	print_binary((1 << 10) + 1);
+	_putchar('\n');
+
+	return (0);
+}
