f9bddba86b46c40f6aea00767904716e8ce02b8c
Henry <adinoyiotaru@gmail.com>
2023-08-07 14:14:47 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,34 +4,51 @@
 #include <fcntl.h>
 
 /**
- * read_textfile - Read and print the contents of a text file to standard output.
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
  *
- * Return: The actual number of letters read and printed, or 0 on failure.
+ * Return: The actual number of letters read and printed.
+ *         0 if the file can't be opened or read, or if filename is NULL,
+ *         or if write fails to print the expected amount of bytes.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t file_descriptor, read_count, write_count;
-	char buffer[1024]; /* A buffer to read data from the file */
+	int fd;
+	ssize_t read_bytes, write_bytes;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	/* Open the file in read-only mode */
-	file_descriptor = open(filename, O_RDONLY);
-	if (file_descriptor == -1)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
 
-	/* Read and print the contents of the file */
-	read_count = read(file_descriptor, buffer, letters);
-	write_count = write(STDOUT_FILENO, buffer, read_count);
-
-	/* Close the file descriptor */
-	close(file_descriptor);
-
-	if (read_count == -1 || write_count == -1 || write_count != read_count)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buffer);
 		return (0);
+	}
 
-	return (read_count);
+	read_bytes = read(fd, buffer, letters);
+	if (read_bytes == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	write_bytes = write(STDOUT_FILENO, buffer, read_bytes);
+	if (write_bytes == -1 || write_bytes != read_bytes)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+	return (write_bytes);
 }
